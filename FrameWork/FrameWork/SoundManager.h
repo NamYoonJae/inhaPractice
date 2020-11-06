925cba52ad013087fecf8269317251d554d353f2
chahyeonbeen <ckgusqls00@gmail.com>
2020-11-06 12:07:24 +0900

    cha // soundManager 3D sfx sound , volume update

--- FrameWork/FrameWork/SoundManager.h
+++ FrameWork/FrameWork/SoundManager.h
@@ -11,31 +11,36 @@
 #define SOUND_DEFAULT 0.5f
 #define SOUND_WEIGHT 0.1f
 
+#define g_pSoundManager cSoundManager::GetInstance()
+
 using namespace FMOD;
 
 class cSoundManager
 {
 private:
+	Singletone(cSoundManager);
 	System * m_fmodSystem;
-
 	Channel* m_cbgmChannel;
 
 	Sound* m_sbgm;
 	map<string, Sound*> m_mapsoundHash;
-
+	float	m_fVolume;
 public:
-	cSoundManager();
-	~cSoundManager();
-
 	void init();
-	void AddSFX(string path, string musicName);
+	void AddSFX(string path, string soundName);
 	void AddBGM(string path);
 
+	void Add3DSFX(string path, string soundName);
+	
 	void PlaySFX(string soundName);
+	void Play3DSFX(string soundName,FMOD_VECTOR ListenerPos,FMOD_VECTOR ChannelPos,FMOD_VECTOR ListenerVel = FMOD_VECTOR{0,0,0});
 	void PlayBGM();
 
 	void Stop();
 
 	void Destroy();
 	void Update();
+
+	void SetVolume(float fVolume) { m_fVolume = fVolume; }
+	float GetVolume() { return m_fVolume; }
 };
