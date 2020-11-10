3093a9699bfefc5248418a9e624fa09bcb624803
NamYoonJae <nyoonjae@naver.com>
2020-11-10 21:37:31 +0900

    == 필터 복구 ==

--- FrameWork/FrameWork/Button.h
+++ FrameWork/FrameWork/Button.h
@@ -12,7 +12,7 @@ public:
 	void Setup(char* root, char* fileName, D3DXVECTOR3 position, float x, float y, float z);
 	void Update(std::string);
 	void Render();
-	void Render(D3DXVECTOR3 position); //click과 hover 이미지 좌표값만 가져와서 랜더
+	void Render(D3DXVECTOR3 position);
 
 	int GetState();
 	void StateChange(int state);
