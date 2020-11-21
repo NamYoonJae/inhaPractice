f4e653f57d66ca969d337cba3c59f1ffe020ad78
NamYoonJae <nyoonjae@naver.com>
2020-11-21 15:36:06 +0900

    == cFont 클래스 추가 생성 ==

---
+++ FrameWork/FrameWork/cFont.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+#include "cFont.h"
+
+
+cFont::cFont()
+	:m_Color(D3DCOLOR_ARGB(255, 255, 255, 255))
+	,m_Format(DT_LEFT | DT_TOP | DT_NOCLIP)
+	,m_pFont(NULL)
+{
+
+}
+
+
+cFont::~cFont()
+{
+	SafeRelease(m_pFont);
+}
+
+
+void cFont::Setup(string text, eFontType fontType)
+{
+	m_Text = text;
+	m_pFont = g_pFontManager->GetFont(fontType);
+}
+
+void cFont::Update()
+{
+
+}
+
+void cFont::Render()
+{
+	m_pFont->DrawTextA(NULL, m_Text.c_str(), m_Text.length(), &m_Rect, m_Format, m_Color);
+
+}
+
