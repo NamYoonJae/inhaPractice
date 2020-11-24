c31b51759c0083a326dc602fc78dcfdb687bd03e
Plan1h <plan1h@naver.com>
2020-11-24 16:47:24 +0900

    no message

---
+++ FrameWork/FrameWork/GameOverSceneBtnEvent.h
@@ -0,0 +1,6 @@
+#pragma once
+
+
+void RetryGameBtnEvent(EventType message, cPopUp* btn);
+void MainMenuBtnEvent(EventType message, cPopUp* btn);
+void ExitGameBtnEvent(EventType message, cPopUp* btn);
